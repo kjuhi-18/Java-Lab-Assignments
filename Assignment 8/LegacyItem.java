1038f57cb462382bf9b4c9d14253ee179b539cdf
Kunal Jhindal <kunal.jhindal.btech2024@sitpune.edu.in>
2026-04-23 23:43:45 +0530

    Add files via upload

---
+++ Assignment 8/LegacyItem.java
@@ -0,0 +1,14 @@
+public class LegacyItem {
+
+    private int    itemId;
+    private String description;
+
+    public LegacyItem(int itemId, String description) {
+        this.itemId      = itemId;
+        this.description = description;
+    }
+    public void print() {
+        System.out.println("Legacy Item ID  : " + itemId);
+        System.out.println("Description     : " + description);
+    }
+}
